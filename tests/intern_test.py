0de2e744ea1d2f9d288620b0975ea800a71e8012
bramsey <bramsey@captionpartners.com>
2026-06-02 15:10:27 -0500

    breaking tests

--- tests/intern_test.py
+++ tests/intern_test.py
@@ -1,14 +1,14 @@
 def test_lydia():
-    assert 1 == 1
+    assert 1 == 0
 
 
 def test_stephen():
-    assert 1 == 1
+    assert 1 == 0
 
 
 def test_rohan():
-    assert 1 == 1
+    assert 1 == 0
 
 
 def test_razan():
-    assert 1 == 1
+    assert 1 == 0
